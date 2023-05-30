f2c43123b7bada5efea4d131e9d28b9d3340ec6b
malla-lokesh <121810307046@gitam.in>
2023-05-30 22:20:06 +0530

    Splitting code into multiple files

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,13 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState =  { counter: 0, showCounter: true }
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        decrement(state, action) {
-            state.counter = state.counter - action.payload;
-        },
-        increment(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggle(state) {
-            state.showCounter = !state.showCounter
-        }
-    }
-})
-
-const initialAuthState = { isLoggedIn: false }
-
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isLoggedIn = true;
-        },
-        logout(state) {
-            state.isLoggedIn = false;
-        }
-    }
-})
+import counterReducer from './counter';
+import authReducer from './auth';
 
 const centralStore = configureStore({
     reducer: { 
-        counter: counterSlice.reducer,
-        authentication: authSlice.reducer
+        counter: counterReducer,
+        authentication: authReducer
     }
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default centralStore;
